f10428bdc3cba4c004f100371078696ee980e888
Akriti457 <sriwastavamahir@gmail.com>
2025-12-27 23:46:39 +0530

    updated file

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,39 @@
-import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import ReportPage from "./pages/ReportPage";
 import FeedPage from "./pages/FeedPage";
 import IncidentPage from "./pages/IncidentPage";
 import AdminPage from "./pages/AdminPage";
-import './index.css'
 import HomePage from "./pages/home";
 import Navbar from "./Navbar";
+import AdminLogin from "./pages/adminlogin";
+import './index.css'
+
 const App = () => {
+  // State to track if admin is logged in
+  const [adminLogin, setAdminLogin] = useState(false);
+
   return (
     <div>
-      <Navbar/>
-       <main>
+      <Navbar />
+      <main>
         <Routes>
-           <Route path="/" element={<HomePage/>} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/feed" element={<FeedPage />} />
           <Route path="/report" element={<ReportPage />} />
           <Route path="/incident/:id" element={<IncidentPage />} />
-          <Route path="/admin" element={<AdminPage />} />
+
+          {/* Admin route: show AdminPage if logged in, otherwise AdminLogin */}
+          <Route
+            path="/admin"
+            element={
+              adminLogin ? (
+                <AdminPage />
+              ) : (
+                <AdminLogin onSubmit={() => setAdminLogin(true)} />
+              )
+            }
+          />
         </Routes>
       </main>
     </div>
